310e33e33e2177f42069059cf4802e92f05e639d
LaliVillagran <luis.villagran1999@gmail.com>
2022-10-24 17:59:41 -0500

    Agregar archivos de proyecto.

---
+++ BackEnd-ejemplo1/Controllers/CalculosBasController.cs
@@ -0,0 +1,43 @@
+﻿using BackEnd_ejemplo1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackEnd_ejemplo1.Controllers
+{
+    public class CalculosBasController : Controller
+    {
+        // GET: CalculosBas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma(string Num1, string Num2)
+        {
+
+            int res = Convert.ToInt16(Num1) + Convert.ToInt16(Num2);
+            ViewBag.Res = Convert.ToString(res);
+
+            return View();
+        }
+
+        public ActionResult Operas()
+        {
+            OperasBas op = new OperasBas();
+            return View(op);
+        }
+
+        [HttpPost]
+        public ActionResult Operas(OperasBas op)
+        {
+            op.Suma();
+            //ViewBag.Res = op.Res;
+            return View();
+
+        }
+
+    }
+}
---
+++ BackEnd-ejemplo1/Services/PeliculasService.cs
@@ -0,0 +1,34 @@
+﻿using BackEnd_ejemplo1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace BackEnd_ejemplo1.Services
+{
+    public class PeliculasService
+    {
+        public List<Pelicula> ObtenerPelicula()
+        {
+            var pelicula1 = new Pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 150,
+                Pais = "USA",
+                Publicacion = new DateTime(2012, 12, 11)
+            };
+
+            var pelicula2 = new Pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 180,
+                Pais = "USA",
+                Publicacion = new DateTime(2015, 12, 11)
+            };
+            return new List<Pelicula>{pelicula1, pelicula2 };
+        }
+    }
+}
